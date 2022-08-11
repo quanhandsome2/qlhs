c2061758a5a35a59f8132c191381f0fe35a01ae3
quan <lenguyenminhquan51ltv@gmail.com>
2022-08-11 17:10:06 +0700

    make it better

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.herokupostgresql',
+        'NAME': os.path.join(BASE_DIR, 'herokupostgresql'),
     }
 }
 
