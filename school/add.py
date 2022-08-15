d2ab67d10c52453552240d1756a5fa6612d4b7de
quan <lenguyenminhquan51ltv@gmail.com>
2022-08-15 15:52:30 +0700

    h

---
+++ school/add.py
@@ -0,0 +1,30 @@
+from .models import *
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
+import pandas as pd
+
+
+def add_nk(request):
+
+    if request.method == "POST" and request.FILES['myfile3']:
+        # Đọc file vào dataframe
+        myfile = request.FILES['myfile3']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        excel_file = uploaded_file_url
+        empexceldata = pd.read_excel("." + excel_file)
+        df = empexceldata
+        print(df)
+
+        for i in range(len(df)):
+
+            nien_khoa = df.loc[i, 'nien_khoa']
+            nien_khoa_tit = df.loc[i, 'nien_khoa_tit']
+
+            nk = Nien_khoa.objects.create(
+                nien_khoa = nien_khoa,
+                nien_khoa_tit=nien_khoa_tit,
+            )
+            nk.save()
+    return render(request, 'school/admin_import_nk.html')
